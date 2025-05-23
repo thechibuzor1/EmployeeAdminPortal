5e9e963992dfc15888ea379667128327ada39756
Chibuzor <chibuzorm26@gmail.com>
2025-05-23 22:17:01 +0100

    ##Add employee management API endpoints and DTO
    Updated `EmployeesController.cs` to include new API methods for retrieving, updating, and deleting employee records by ID and name. Added `GetEmployeeById`, `GetEmployeeByName`, `UpdateEmployee`, and `DeleteEmployee` methods.
    
    Introduced `UpdateEmployeeDTO` class in `updateEmployeeDTO.cs` to define properties for updating employee information, with required fields for `Name` and `Email`.

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -28,6 +28,33 @@ namespace EmployeeAdminPortal.Controllers
 
         }
 
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee =  dbContext.Employees.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound("Employee not found");
+            }
+            return Ok(employee);
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        public IActionResult GetEmployeeByName(string name)
+        {
+            var employee = dbContext.Employees.FirstOrDefault(e => e.Name == name);
+            if (employee is null)
+            {
+                return NotFound("Employee not found");
+            }
+            return Ok(employee);
+        }
+                        
+        
         [HttpPost]
         public IActionResult CreateEmployee(CreateEmployeeDTO createEmployeeDTO)
         {
@@ -43,6 +70,48 @@ namespace EmployeeAdminPortal.Controllers
             dbContext.SaveChanges();
 
             return Ok(employee);
+        }
+
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDTO updateEmployeeDTO)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound("Employee not found");
+            }
+
+
+            employee.Name = updateEmployeeDTO.Name;
+            employee.Email= updateEmployeeDTO.Email;
+            employee.Phone = updateEmployeeDTO.Phone;
+            employee.Salary = updateEmployeeDTO.Salary;
+
+            dbContext.SaveChanges();
+
+            return Ok(employee);
+
+        }
+
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound("Employee not found");
+            }
+
+            dbContext.Employees.Remove(employee);
+            dbContext.SaveChanges();
+
+            return Ok("Employee deleted successfully");
         }
 
     }
